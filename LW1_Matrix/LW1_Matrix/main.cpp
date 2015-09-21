522862edb07061ca9df75e119f19e16f067a7b27
Dmitry Sukholovsky <dmitrij.96s@gmail.com>
2015-09-21 22:07:13 +0300

    7yt7ygyu

--- LW1_Matrix/LW1_Matrix/main.cpp
+++ LW1_Matrix/LW1_Matrix/main.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 //#include "list.h"
 #include "commandExecutor.h"
 #define FILE "input.dat"
+#include "string"
 
 int main() {
 	CommandExecutor commandExecutor("input.dat");
-	//system("Pause");
+	system("Pause");
 	return 0;
 }
